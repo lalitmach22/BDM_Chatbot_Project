d74f13c23f27f1059f542789887cb71c37745e10
LalitIITM <21f3001013@ds.study.iitm.ac.in>
2024-12-30 19:02:23 +0530

    d

--- backend/local_setup.sh
+++ backend/local_setup.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 echo "======================================================================"
-echo "Welcome to to the setup. This will setup the local virtual env." 
+echo "Welcome to the setup. This will set up the local virtual env."
 echo "And then it will install all the required python libraries."
 echo "You can rerun this without any issues."
 echo "----------------------------------------------------------------------"
+
 # Measure the time before installing Python libraries
 start_time=$(date +%s)
 
-if [ -d ".venv" ];
-then
+if [ -d ".venv" ]; then
     echo ".venv folder exists. Installing using pip"
 else
     echo "creating .venv and install using pip"
@@ -19,11 +19,10 @@ fi
 echo "Activating virtual environment"
 . .venv/bin/activate
 
-# Upgrade the PIP
+# Upgrade the PIP (if you uncomment this)
 #echo "Upgrading PIP..."
 #pip install --upgrade pip
-#echo "Installing Python libraries from requirements.txt..."
-pip install Flask
+echo "Installing Python libraries from requirements.txt..."
 pip install -r requirements.txt
 
 # Print installed packages
@@ -32,12 +31,14 @@ echo "Installed Python packages:"
 pip freeze
 echo "======================================================================"
 
+# Measure the time after installation
 end_time=$(date +%s)
-
-# Calculate and display the elapsed time
 elapsed_time=$((end_time - start_time))
 echo "Installation completed in $elapsed_time seconds."
-#run the app
-python3 main.py
-# Work done. so deactivate the virtual env
+
+# Run the app
+echo "Running main.py..."
+python3 main.py || { echo "Failed to start main.py"; exit 1; }
+
+# Work done, deactivate virtual env
 deactivate
